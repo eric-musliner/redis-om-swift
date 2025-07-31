d702795cda524dc09fb8d3f18e25c28a737319ff
Eric Musliner <eric.musliner@gmail.com>
2025-07-31 16:08:12 -0400

    Adds Vapor and VaporTesting dependencies
     - used for Vapor LifecycleHandler

--- Package.swift
+++ Package.swift
@@ -13,6 +13,8 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/swift-server/RediStack.git", from: "1.4.1"),
         .package(url: "https://github.com/swift-server/swift-service-lifecycle.git", from: "2.8.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.115.0"),
+
     ],
     targets: [
         .target(
@@ -20,12 +22,14 @@ let package = Package(
             dependencies: [
                 .product(name: "RediStack", package: "RediStack"),
                 .product(name: "ServiceLifecycle", package: "swift-service-lifecycle"),
+                .product(name: "Vapor", package: "vapor")
             ]
         ),
         .testTarget(
             name: "RedisOMTests",
             dependencies: [
                 .target(name: "RedisOM"),
+                .product(name: "VaporTesting", package: "vapor")
             ]
         ),
     ]
