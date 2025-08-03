143398b439ccb1a5d35580cb2477219bceb55273
Eric Musliner <eric.musliner@gmail.com>
2025-08-03 19:51:28 -0400

    Updates withFlushedRedis test helper
     - fixes functionality to actually execute and await body of test
     - always executes final flush in a defer task
     - flush redis db after every test
     - swift testing weirdly doesn't support an async teardown api

--- Tests/RedisOMTests/Helpers/withFlushedRedis.swift
+++ Tests/RedisOMTests/Helpers/withFlushedRedis.swift
@@ -3,11 +3,12 @@ import Testing
 @testable import RedisOM
 
 /// Test helper to flush redis after test run
-func withFlushedRedis(_ body: @Sendable () async throws -> Void) async throws {
-    do {
+func withFlushedRedis(_ body: () async throws -> Void) async throws {
+    defer {
         Task {
             let client = await SharedPoolHelper.shared()
-            _ = client.send(command: "FLUSHALL")
+            _ = try await client.send(command: "FLUSHALL").get()
         }
     }
+    try await body()
 }
