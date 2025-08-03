83cb8e1facdf7bea4edbd08d4f78edac6d709d2c
Eric Musliner <eric.musliner@gmail.com>
2025-08-03 19:48:53 -0400

    Adds allKeys capability to JsonModel
     - Get all keys for a given model in redis

--- Sources/RedisOM/RedisModel.swift
+++ Sources/RedisOM/RedisModel.swift
@@ -87,6 +87,49 @@ extension JsonModel {
         ).get()
     }
 
+    /// Get list of Primary Keys for model in Redis
+    /// - Returns:
+    ///    - [IDType]: list of primary keys
+    @inlinable
+    public static func allKeys() async throws -> [IDType] {
+        let client = await SharedPoolHelper.shared()
+        var cursor = "0"
+        var allKeys: [IDType] = []
+
+        repeat {
+            let response = try await client.send(
+                command: "SCAN",
+                with: [
+                    .bulkString(ByteBuffer(string: cursor)),
+                    .bulkString(ByteBuffer(string: "MATCH")),
+                    .bulkString(ByteBuffer(string: "\(keyPrefix):*")),
+                    .bulkString(ByteBuffer(string: "COUNT")),
+                    .bulkString(ByteBuffer(string: "100")),
+                ]
+            ).get()
+
+            guard
+                let array = response.array,
+                array.count == 2,
+                let nextCursor = array[0].string,
+                let keysArray = array[1].array
+            else {
+                throw RedisError(reason: "Invalid SCAN response structure")
+            }
+
+            cursor = nextCursor
+            for keyResp in keysArray {
+                if let fullKey = keyResp.string {
+                    if let pk = fullKey.split(separator: ":").last {
+                        allKeys.append(IDType(String(pk))!)
+                    }
+                }
+            }
+        } while cursor != "0"
+
+        return allKeys
+    }
+
     /// Get model by primary key
     /// - Parameters:
     ///    - id: primary key of saved model in Redis
