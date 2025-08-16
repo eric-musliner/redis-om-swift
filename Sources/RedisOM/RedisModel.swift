9fa275da2c140c75223b230cd38b87639264d811
Eric Musliner <eric.musliner@gmail.com>
2025-08-16 15:01:11 -0400

    Creates directory for property wrappers

--- Sources/RedisOM/RedisModel.swift
+++ Sources/RedisOM/RedisModel.swift
@@ -170,24 +170,3 @@ extension JsonModel {
     }
 
 }
-
-// MARK: - Property Wrapper
-@propertyWrapper
-public struct AutoID: Codable, Sendable {
-    public var wrappedValue: String?
-
-    public init(wrappedValue: String? = nil) {
-        self.wrappedValue = wrappedValue ?? UUID().uuidString
-    }
-
-    public init(from decoder: Decoder) throws {
-        let container = try decoder.singleValueContainer()
-        let decoded = try? container.decode(String.self)
-        self.wrappedValue = decoded?.isEmpty == false ? decoded! : UUID().uuidString
-    }
-
-    public func encode(to encoder: Encoder) throws {
-        var container = encoder.singleValueContainer()
-        try container.encode(wrappedValue)
-    }
-}
