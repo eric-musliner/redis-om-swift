6a1f6a81c9480f964d1331656e8a4bd57f2390b5
Eric Musliner <eric.musliner@altamiracorp.com>
2025-09-17 11:54:45 -0400

    Adds rest of code from RedisConnectionPoolService
     - Adds extension on RedisConnectionPoolService from the
       hummingbird-redis project.
     - Will add notice for usage but this is to keep as little modifications
       as possible to original

--- Sources/RedisOM/Connection/RedisConnectionPoolService.swift
+++ Sources/RedisOM/Connection/RedisConnectionPoolService.swift
@@ -1,9 +1,27 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Hummingbird server framework project
+//
+// Copyright (c) 2021-2023 the Hummingbird authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See hummingbird/CONTRIBUTORS.txt for the list of Hummingbird authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
 import Foundation
 import Logging
 import NIO
 import NIOCore
 @preconcurrency import RediStack
 import ServiceLifecycle
+
+// Based on Hummingbird Redis ConnectionPoolService.
+// https://github.com/hummingbird-project/hummingbird-redis/blob/main/Sources/HummingbirdRedis/RedisConfiguration.swift
+// Modifications made to initialization and member attributes
 
 /// Redis Connection Pool Service to manage creation and configuration of Redis Connection Pool
 public struct RedisConnectionPoolService: Sendable {
@@ -70,6 +88,66 @@ public struct RedisConnectionPoolService: Sendable {
         let promise = self.eventLoop.makePromise(of: Void.self)
         self.connectionPool.close(promise: promise)
         return try await promise.futureResult.get()
+    }
+}
+
+extension RedisConnectionPoolService {
+    /// A unique identifer to represent this connection.
+    @inlinable
+    public var id: UUID { self.connectionPool.id }
+    /// The count of connections that are active and available for use.
+    @inlinable
+    public var availableConnectionCount: Int { self.connectionPool.availableConnectionCount }
+    /// The number of connections that have been handed out and are in active use.
+    @inlinable
+    public var leasedConnectionCount: Int { self.connectionPool.leasedConnectionCount }
+    /// Provides limited exclusive access to a connection to be used in a user-defined specialized closure of operations.
+    /// - Warning: Attempting to create PubSub subscriptions with connections leased in the closure will result in a failed `NIO.EventLoopFuture`.
+    ///
+    /// `RedisConnectionPool` manages PubSub state and requires exclusive control over creating PubSub subscriptions.
+    /// - Important: This connection **MUST NOT** be stored outside of the closure. It is only available exclusively within the closure.
+    ///
+    /// All operations should be done inside the closure as chained `NIO.EventLoopFuture` callbacks.
+    ///
+    /// For example:
+    /// ```swift
+    /// let countFuture = pool.leaseConnection {
+    ///     let client = $0.logging(to: myLogger)
+    ///     return client.authorize(with: userPassword)
+    ///         .flatMap { connection.select(database: userDatabase) }
+    ///         .flatMap { connection.increment(counterKey) }
+    /// }
+    /// ```
+    /// - Warning: Some commands change the state of the connection that are not tracked client-side,
+    /// and will not be automatically reset when the connection is returned to the pool.
+    ///
+    /// When the connection is reused from the pool, it will retain this state and may affect future commands executed with it.
+    ///
+    /// For example, if `select(database:)` is used, all future commands made with this connection will be against the selected database.
+    ///
+    /// To protect against future issues, make sure the final commands executed are to reset the connection to it's previous known state.
+    /// - Parameter operation: A closure that receives exclusive access to the provided `RedisConnection` for the lifetime of the closure for specialized Redis command chains.
+    /// - Returns: A `NIO.EventLoopFuture` that resolves the value of the `NIO.EventLoopFuture` in the provided closure operation.
+    @inlinable
+    public func leaseConnection<T>(_ operation: @escaping (RedisConnection) -> EventLoopFuture<T>)
+        -> EventLoopFuture<T>
+    {
+        self.connectionPool.leaseConnection(operation)
+    }
+
+    /// Updates the list of valid connection addresses.
+    /// - Warning: This will replace any previously set list of addresses.
+    /// - Note: This does not invalidate existing connections: as long as those connections continue to stay up, they will be kept by
+    /// this client.
+    ///
+    /// However, no new connections will be made to any endpoint that is not in `newAddresses`.
+    /// - Parameters:
+    ///     - newAddresses: The new addresses to connect to in future connections.
+    ///     - logger: An optional logger to use for any log statements generated while updating the target addresses.
+    ///         If one is not provided, the pool will use its default logger.
+    @inlinable
+    public func updateConnectionAddresses(_ newAddresses: [SocketAddress], logger: Logger? = nil) {
+        self.connectionPool.updateConnectionAddresses(newAddresses)
     }
 }
 
