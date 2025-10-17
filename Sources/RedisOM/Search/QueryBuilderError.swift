20abaae0f7b04accd2c488029443b7c64345f14d
Eric Musliner <eric.musliner@altamiracorp.com>
2025-10-17 12:39:37 -0400

    Removes custom fieldNotIndexed error
     - Now that queries are built using custom static fields from
       macro expansion and not keyPaths on model attributes there
       doesn't need to be a runtime check for if the field being
       queried is indexed

--- Sources/RedisOM/Search/QueryBuilderError.swift
+++ Sources/RedisOM/Search/QueryBuilderError.swift
@@ -1,11 +1,8 @@
 enum QueryBuilderError: Error, CustomStringConvertible {
-    case fieldNotIndexed(field: String)
     case invalidType(field: String, actual: String, expected: String)
 
     var description: String {
         switch self {
-        case .fieldNotIndexed(let field):
-            return "Tried to query on non-indexed field: \(field)"
         case .invalidType(let field, let actual, let expected):
             return "Invalid index type \(actual) for field: \(field), expected: \(expected)"
         }
